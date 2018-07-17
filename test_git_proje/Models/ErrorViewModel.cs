0edaa257c13e264da2a663a6be6b9e1014194c00
İrfan Büyükyangöz <irfanbuyukyangoz@hotmail.com>
2018-07-17 11:07:52 +0300

    ilk proje yüklemesi

---
+++ test_git_proje/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_git_proje.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
